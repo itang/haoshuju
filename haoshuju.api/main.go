2f356c513a43bf0ba77d9bcf518a42c9ad191beb
itang <live.tang@gmail.com>
2013-12-02 19:54:47 +0800

    set GOMAXPROCS

--- haoshuju.api/main.go
+++ haoshuju.api/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"runtime"
+
 	"github.com/itang/haoshuju/haoshuju.api/api"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	var server api.Server = api.NewMartiniServer()
 	server.Run()
 }
